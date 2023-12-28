1e29bf93cdbfa3662421954ed621fe748f1fab48
LiChaoYang <249313573@qq.com>
2023-12-28 10:19:01 +0800

    shader

--- src/pages/Shader/index.vue
+++ src/pages/Shader/index.vue
@@ -6,11 +6,112 @@
 import { onMounted } from 'vue'
 import * as THREE from 'three'
 import GUI from 'lil-gui'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import ImgFlagFrench from './flag-french.jpg'
 
 const uniqueId = `three-${Math.random().toString(36).slice(2)}`
 
 onMounted(() => {
+    /**
+     * Base
+     */
+    // Debug
     const gui = new GUI({})
+
+    // Canvas
+    const elemCanvas = document.querySelector(`#${uniqueId}`) as HTMLElement
+    console.log('elemCanvas', elemCanvas)
+
+    // Scene
+    const scene = new THREE.Scene()
+    scene.background = new THREE.Color(0x000055)
+
+    // Light
+    const color = 0xffffff
+    const intensity = 1
+    const light = new THREE.DirectionalLight(color, intensity)
+    light.position.set(-1, 2, 4)
+    scene.add(light)
+
+    const lightHelper = new THREE.DirectionalLightHelper(light)
+    scene.add(lightHelper)
+
+    // Textures
+    const textureLoader = new THREE.TextureLoader()
+    const flagTexture = textureLoader.load(ImgFlagFrench)
+
+    /**
+     * Test mesh
+     */
+    // Geometry
+    const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
+    const count = geometry.attributes.position.count
+    const randoms = new Float32Array(count)
+
+    for (let i = 0; i < count; i++) {
+        randoms[i] = Math.random()
+    }
+
+    geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1))
+
+    // Material
+    const material = new THREE.ShaderMaterial({
+        vertexShader: `uniform vec2 uFrequency;
+uniform float uTime;
+
+varying vec2 vUv;
+varying float vElevation;
+
+void main()
+{
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    float elevation = cos(modelPosition.x * uFrequency.x - uTime) * 0.1;
+    elevation += cos(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+    // modelPosition.x += elevation * 0.2;
+    // modelPosition.y += elevation * 0.2;
+    modelPosition.z += cos(modelPosition.x * uFrequency.x - uTime) * 0.1;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
+
+    vUv = uv;
+    vElevation = elevation;
+}`,
+        fragmentShader: `uniform vec3 uColor;
+        uniform sampler2D uTexture;
+
+        varying vec2 vUv;
+        varying float vElevation;
+
+        void main()
+        {
+            // vec4 textureColor = texture2D(uTexture, vUv);
+            // textureColor.rgb *= vElevation * 2.0 + 0.55;
+            // gl_FragColor = textureColor;
+            gl_FragColor = vec4(vUv, 1.0, 1.0);
+        }`,
+        side: THREE.DoubleSide,
+        uniforms: {
+            uFrequency: { value: new THREE.Vector2(10, 5) },
+            uTime: { value: 0 },
+            uColor: { value: new THREE.Color('red') },
+            uTexture: { value: flagTexture },
+        },
+    })
+    gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01)
+    gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01)
+    gui.add(material.uniforms.uColor.value, 'r').min(0).max(1).step(0.01)
+    gui.add(material.uniforms.uColor.value, 'g').min(0).max(1).step(0.01)
+    gui.add(material.uniforms.uColor.value, 'b').min(0).max(1).step(0.01)
+
+    // Mesh
+    const mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
+
     const sizes = {
         // width: 800,
         // height: 600,
@@ -31,14 +132,11 @@ onMounted(() => {
         renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
     })
 
-    const container = document.querySelector(`#${uniqueId}`) as HTMLElement
     const renderer = new THREE.WebGLRenderer({
-        canvas: container,
+        canvas: elemCanvas,
         antialias: true,
     })
     renderer.setSize(sizes.width, sizes.height)
-    renderer.shadowMap.enabled = true
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
     const cursor = {
         x: 0,
@@ -49,15 +147,43 @@ onMounted(() => {
         cursor.y = -(e.clientY / sizes.height - 0.5)
     })
 
-    const scene = new THREE.Scene()
-    scene.background = new THREE.Color(0x000000)
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        sizes.width / sizes.height,
+        0.1,
+        100
+    )
+    camera.position.z = 5
+    const cameraHelper = new THREE.CameraHelper(camera)
+    scene.add(cameraHelper)
+    // const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 
-    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+    // Controls
+    const controls = new OrbitControls(camera, elemCanvas)
+    controls.enableDamping = true
+
+    /**
+     * Animate
+     */
+    const clock = new THREE.Clock()
 
     function tick() {
-        camera.updateProjectionMatrix()
+        const elapsedTime = clock.getElapsedTime()
+
+        // Update material
+        material.uniforms.uTime.value = elapsedTime
+
+        // Update controls
+        controls.update()
+
+        // Render
         renderer.render(scene, camera)
-        requestAnimationFrame(tick)
+
+        // Call tick again on the next frame
+        window.requestAnimationFrame(tick)
+        // camera.updateProjectionMatrix()
+        // renderer.render(scene, camera)
+        // requestAnimationFrame(tick)
     }
     tick()
 })
